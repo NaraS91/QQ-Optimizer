38dc60520da5e5d0d4ea48aec7aefe7ee7b7899f
NaraS91 <39410439+NaraS91@users.noreply.github.com>
2024-06-24 23:43:25 +0100

    oops

--- src/app/hsr.rs
+++
@@ -1,5 +0,0 @@
-pub mod basics;
-pub mod damange_calculator;
-pub mod light_cones;
-pub mod relics;
-pub mod units;
--- src/app/hsr/relics/genius_of_brilliant_stars.rs
+++
@@ -1,6 +0,0 @@
-use super::{relics, Modifier};
-use enum_map::Enum;
-
-fn modifiers(set: RelicSet) -> Vec<ModifierData> {
-    vec![ModifierData { c }]
-}
